d73d91639ff0cd081d19f02e2a7a1f4b1bd2098c
notehappy <72262766+notehappy@users.noreply.github.com>
2023-03-13 15:18:40 +0700

    Update main_app_2.py

--- main_app_2.py
+++ main_app_2.py
@@ -3,12 +3,12 @@ import folium
 import pandas as pd
 from streamlit_folium import folium_static
 st.set_page_config(layout ="wide")
-json1 = fr"D:\2021_DataScience\2023_streamlit\SDG_app\Data\tha_admbnda_adm1_rtsd_201902_WGS.json"
+json1 = "Data/tha_admbnda_adm1_rtsd_201902_WGS.json"
 
 m = folium.Map(location=[23.47,77.94], tiles='CartoDB positron', name="Light Map",
                zoom_start=5, attr="My Data attribution")
 
-df = r"D:\2021_DataScience\2023_streamlit\SDG_app\Data\sdg_monthly_TH(pro).xlsx"
+df = r"Data/sdg_monthly_TH(pro).csv"
 df = pd.read_csv(df)
 
 choice = ['2018-02-01 00:00:00', '2018-03-01 00:00:00']
