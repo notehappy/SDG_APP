5a2e3bc2b2ce30f69cfa0c39bd115b232a1803fe
notehappy <pongsakon.punpuk@gmail.com>
2023-03-13 16:48:57 +0700

    version 3

--- main_app.py
+++ main_app.py
@@ -2,31 +2,38 @@ import streamlit as st
 import folium
 import pandas as pd
 from streamlit_folium import folium_static
-st.set_page_config(layout ="wide")
-json1 = fr"D:\2021_DataScience\2023_streamlit\SDG_app\Data\tha_admbnda_adm1_rtsd_201902_WGS.json"
-# Add title and header
-st.title("Renewable Energy Production in Switzerland")
-st.header("Energy Production by Cantons (MWH) ")
+json1 = r"Data/tha_admbnda_adm1_rtsd_201902_WGS.json"
+
 m = folium.Map(location=[23.47,77.94], tiles='CartoDB positron', name="Light Map",
                zoom_start=5, attr="My Data attribution")
 
-df = pd.read_excel(r'D:\2021_DataScience\2023_streamlit\SDG_app\Data\sdg_monthly_TH(pro).xlsx')
+df = r"Data/sdg_monthly_TH(pro).csv"
+df = pd.read_csv(df)
 
-# choice = ['2018-02-01 00:00:00', '2018-03-01 00:00:00']
-# choice_selected = st.selectbox("Select choice", choice)
+choice = ['February 2018', 'March 2018', 'April 2018', 'May 2018',
+       'June 2018', 'July 2018', 'August 2018', 'September 2018',
+       'October 2018', 'November 2018', 'December 2018', 'January 2019',
+       'February 2019', 'March 2019', 'April 2019', 'May 2019', 'June 2019',
+       'July 2019', 'August 2019', 'September 2019', 'October 2019',
+       'November 2019', 'December 2019', 'January 2020', 'February 2020',
+       'March 2020', 'April 2020', 'May 2020', 'June 2020', 'July 2020',
+       'August 2020', 'September 2020', 'October 2020', 'November 2020',
+       'December 2020', 'January 2021', 'February 2021', 'March 2021',
+       'April 2021', 'May 2021', 'June 2021', 'July 2021', 'August 2021',
+       'September 2021', 'October 2021', 'November 2021', 'December 2021']
+choice_selected = st.selectbox("Select choice", choice)
 
 folium.Choropleth(
     geo_data=json1,
     name="choropleth",
     data=df,
-    columns=["ADM1_EN",'test'],
+    columns=["ADM1_EN",choice_selected],
     key_on="feature.properties.ADM1_EN",
     fill_color="YlOrRd",
     fill_opacity=0.7,
     line_opacity=.1,
-    legend_name='2018-02-01 00:00:00'
+    legend_name=choice_selected
 ).add_to(m)
-
 folium.features.GeoJson(json1,
                         name="ADM1_EN", popup=folium.features.GeoJsonPopup(fields=["ADM1_EN"])).add_to(m)
 
--- main_app_v3.py
+++ main_app_v3.py
@@ -1,17 +1,29 @@
-import streamlit as st
-import folium
 import pandas as pd
-from streamlit_folium import folium_static
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+from copy import deepcopy
+from plotly.subplots import make_subplots
+from PIL import Image
+import os
 
 json1 = "Data/tha_admbnda_adm1_rtsd_201902_WGS.json"
-
-m = folium.Map(location=[13.72917,100.52389], tiles='CartoDB positron', name="Light Map",
-               zoom_start=5, attr="My Data attribution")
+with open(json1) as response:
+    geo = json.load(response)
 
 df = r"Data/sdg_monthly_TH(pro).csv"
 df = pd.read_csv(df)
+
+image = Image.open(r'Image/AIT_logo.png')
+st.image(image, caption='Sunrise by the mountains', width=400)
 st.title("An assessment of Thailand's progress towards achieving SDG indicator 11.6.2 for the period 2018 - 2021")
 st.header("Spatial distribution of SDG 11.6.2 by monthly average and provinces in Thailand")
+
+
+
 choice = ['February 2018', 'March 2018', 'April 2018', 'May 2018',
        'June 2018', 'July 2018', 'August 2018', 'September 2018',
        'October 2018', 'November 2018', 'December 2018', 'January 2019',
@@ -24,19 +36,27 @@ choice = ['February 2018', 'March 2018', 'April 2018', 'May 2018',
        'April 2021', 'May 2021', 'June 2021', 'July 2021', 'August 2021',
        'September 2021', 'October 2021', 'November 2021', 'December 2021']
 choice_selected = st.selectbox("Select choice", choice)
+# Geographic Map
+fig = go.Figure(
+    go.Choroplethmapbox(
+        geojson= geo,
+        locations=df['ADM1_EN'],
+        featureidkey="properties.ADM1_EN",
+        z=df[choice_selected],
+        colorscale="sunsetdark",
+        # zmin=0,
+        # zmax=500000,
+        marker_opacity=0.5,
+        marker_line_width=0,
+    )
+)
+fig.update_layout(
+    mapbox_style="carto-positron",
+    mapbox_zoom=4.8,
+    mapbox_center={"lat": 13.72917, "lon": 100.52389},
+    width=800,
+    height=600,
+)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+st.plotly_chart(fig)
 
-folium.Choropleth(
-    geo_data=json1,
-    name="choropleth",
-    data=df,
-    columns=["ADM1_EN",choice_selected],
-    key_on="feature.properties.ADM1_EN",
-    fill_color="YlOrRd",
-    fill_opacity=0.7,
-    line_opacity=.1,
-    legend_name=choice_selected
-).add_to(m)
-folium.features.GeoJson(json1,
-                        name="ADM1_EN", popup=folium.features.GeoJsonPopup(fields=["ADM1_EN"])).add_to(m)
-
-folium_static(m, width=1600, height=950)
