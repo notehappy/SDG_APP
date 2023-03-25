3b8e6f9f9871654329038bebec97602d62599ea9
notehappy <pongsakon.punpuk@gmail.com>
2023-03-25 23:53:43 +0700

    version3

--- pages/SDG_11_2_1.py
+++ pages/SDG_11_2_1.py
@@ -130,7 +130,7 @@ elif choice_selected1 == 'normal person' and choice_selected2 == 'percentage of 
 
 elif choice_selected1 == 'normal person' and choice_selected2 == 'number of people':
     left_column1, right_column1 = st.columns([1, 1])
-    choice3 = ['All public transportation', 'Bus', 'Ferry', 'Railway','Train']
+    choice3 = ['All public transportation', 'Bus','Railway']
     choice_selected3 = left_column1.selectbox("Select the type of public transportration", choice3)
     choice4 = ['Children', 'Adults', 'Older Adults', 'Nonidentified']
     choice_selected4 = right_column1.selectbox("Select the age ranges", choice4)
@@ -161,7 +161,7 @@ elif choice_selected1 == 'normal person' and choice_selected2 == 'number of peop
     
 elif choice_selected1 == 'disabled person' and choice_selected2 == 'percentage of people':
     left_column1, right_column1 = st.columns([1, 1])
-    choice3 = ['All public transportation', 'Bus', 'Ferry', 'Railway','Train']
+    choice3 = ['All public transportation', 'Bus', 'Railway']
     choice_selected3 = left_column1.selectbox("Select the type of public transportration", choice3)
     df = percent_normanl
     st.write('The percentage of people can access to public transportration in BMR during 2020')
