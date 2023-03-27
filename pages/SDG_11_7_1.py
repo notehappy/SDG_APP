252fd0d6000c645f0670d3d371fce82c79326d63
notehappy <pongsakon.punpuk@gmail.com>
2023-03-27 10:29:32 +0700

    Update SDG_11_7_1.py

--- pages/SDG_11_7_1.py
+++ pages/SDG_11_7_1.py
@@ -18,3 +18,21 @@ from PIL import Image
 import os
 st.set_page_config(layout="wide", page_title='Ratio of land consumption rate to population growth rate', page_icon=":earth_asia:")
 st.sidebar.markdown("SDG 11.3.1 Assessment")
+
+# =============================================================================
+# Banner and Title
+# =============================================================================
+image = r'Image/banner.png'
+st.image(image)
+st.title("PM2.5 concentration in Thailand using PM2.5 model of study")
+
+# =============================================================================
+# Description
+# =============================================================================
+st.subheader('Description')
+# Description of the website
+st.write("* This page presents the evaluation of indicators of sustainable development. Specifically, it focuses on Indicator 11.7.1, which is funded by the National Research Council of Thailand. ")
+st.write("* All of data was collected by Bangkok Metropolitan Administration (BMA)")
+# Information about the researchers
+st.write("* The researchers responsible for Indicator 11.7.1 were Associate Professor Dr. Ekebodin Winijkul and Mr. Pongsakorn Punpukdee.")
+st.write("* For more information and detail contact: ekbordinw@ait.asia")
