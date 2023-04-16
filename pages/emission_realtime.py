479754ca0ac6b3f2b6187533f9c6017484ea0d88
notehappy <pongsakon.punpuk@gmail.com>
2023-04-16 15:09:49 +0700

    linuix

--- pages/emission_realtime.py
+++ pages/emission_realtime.py
@@ -277,96 +277,106 @@ st.warning('Caution: The spatial map may take some time to process and may resul
 left_column4, right_column4 = st.columns([1, 1])
 choice5 = db.index.unique()
 choice5 = choice5.sort_values(ascending=False)
-choice_selected5 = left_column4.selectbox("Select time for show distribution", choice5, key='option3')
+choice_selected5 = left_column4.selectbox("Select time for show distribution", choice5)
 choice6 = db.columns[1:]
-choice_selected6 = right_column4.selectbox("Select air pollutant types", choice6, key='option4')
+choice_selected6 = right_column4.selectbox("Select air pollutant types", choice6, key='option2')
 db1 = db.loc[choice_selected5]
 
-db2 = db1
-db2.drop('Id', axis = 1, inplace = True)
-db2 = pd.DataFrame(db2.sum(), columns=['emisson (Kg)'])
+a = f'{type(db1)}'
+if a == "<class 'pandas.core.series.Series'>":
+    db2 = pd.DataFrame(db1[1:])
+    db2.columns = ['emisson (Kg)']
+elif a == "<class 'pandas.core.frame.DataFrame'>":
+    db2 = db1.copy()
+    db2.drop(columns = 'Id', axis = 1, inplace = True)
+    db2 = pd.DataFrame(db2.sum(), columns=['emisson (Kg)'])
+
+if f"{type(db1[choice_selected6])}" == "<class 'numpy.float64'>": #selecting 6 instead of CO
+    z = pd.Series(db1[choice_selected6], index=[choice_selected5])
+else:
+    z = db1[choice_selected6]
 # Geographic Map
 st.write(f'{style_title_graph}<p class="center-text bold-color-text">"{choice_selected6} Emissions from Active Fires Detected by HIMAWARI Sensor in Lampang on {choice_selected5}"</p>', unsafe_allow_html=True)
-# left_column5, right_column5 = st.columns([1, 1])
-# with left_column5:
-#     fig3 = go.Figure(
-#         go.Choroplethmapbox(
-#             geojson= geo,
-#             locations=db['Id'],
-#             featureidkey="properties.Id",
-#             z=db1[choice_selected6],
-#             colorscale="sunsetdark",
-#             # zmin=0,
-#             # zmax=500000,
-#             marker_opacity=0.5,
-#             marker_line_width=0,
-#             name = f'{choice_selected6} Emissions from Active Fires Detected by HIMAWARI Sensor in Lampand on {choice_selected5}',
-#             colorbar=dict(title="Unit of Kg")
-#         )
-#     )
-#     fig3.update_layout(
-#         paper_bgcolor="#E3E3E3",
-#         mapbox_style="carto-positron",
-#         mapbox_zoom=7,
-#         mapbox_center={"lat": 18.34, "lon": 99.5},
-#         # width=800,
-#         # height=600,
-#         font=dict(color='black')
-#     )
-#     fig3.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-#     st.plotly_chart(fig3, use_container_width=True)
+left_column5, right_column5 = st.columns([1, 1])
+with left_column5:
+    fig3 = go.Figure(
+        go.Choroplethmapbox(
+            geojson= geo,
+            locations=db['Id'],
+            featureidkey="properties.Id",
+            z=z,
+            colorscale="sunsetdark",
+            # zmin=0,
+            # zmax=500000,
+            marker_opacity=0.5,
+            marker_line_width=0,
+            name = f'{choice_selected6} Emissions from Active Fires Detected by HIMAWARI Sensor in Lampand on {choice_selected5}',
+            colorbar=dict(title="Unit of Kg")
+        )
+    )
+    fig3.update_layout(
+        paper_bgcolor="#E3E3E3",
+        mapbox_style="carto-positron",
+        mapbox_zoom=7,
+        mapbox_center={"lat": 18.34, "lon": 99.5},
+        # width=800,
+        # height=600,
+        font=dict(color='black')
+    )
+    fig3.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    st.plotly_chart(fig3, use_container_width=True)
 
-# with right_column5:
-#     fig4 = go.Figure()
-#     for i in range(db2.shape[0]):
-#         fig4.add_trace(
-#             go.Bar(
-#                 x=[db2.index[i]],
-#                 y=[db2.iloc[i,0]],
-#                 hovertemplate="%{y:.2f}",
-#                 name= f'{db2.index[i]}',
-#         ),
-#         )
-#     # fig2.update_layout(barmode="stack")
-#     fig4.update_layout(
-#         paper_bgcolor="#E3E3E3",
-#         plot_bgcolor="#FFFFFF",
-#         # width=900,
-#         # height=1000,
-#         # title={'text' : f"SDG 11.2.1 assessment and related value by selecting province in Thailand"
-#         #     ,'x': 0.5, # Set the x anchor to the center of the chart
-#         #     'xanchor': 'center'},
-#         margin=dict(l=50, r=50, t=50, b=50),
-#         legend=dict(
-#             orientation="h",
-#             yanchor="bottom",
-#             y=1.02,
-#             xanchor="right",
-#             x=1
-#         ),
-#         xaxis_title='Types of emission detected by MODIS ',
-#         yaxis_title='Amount of emission in Kg',
-#         font=dict(
-#             color='black',
-#         ),
-#         xaxis=dict(
-#             title_font=dict(
-#                 color='black',
-#             ),
-#             tickfont=dict(
-#                 color='black',
-#             )
-#         ),
-#         yaxis=dict(
-#             title_font=dict(
-#                 color='black',
-#             ),
-#             tickfont=dict(
-#                 color='black',
-#             )
-#         )
-#     )
-#     st.plotly_chart(fig4, use_container_width=True)
+with right_column5:
+    fig4 = go.Figure()
+    for i in range(db2.shape[0]):
+        fig4.add_trace(
+            go.Bar(
+                x=[db2.index[i]],
+                y=[db2.iloc[i,0]],
+                hovertemplate="%{y:.2f}",
+                name= f'{db2.index[i]}',
+        ),
+        )
+    # fig2.update_layout(barmode="stack")
+    fig4.update_layout(
+        paper_bgcolor="#E3E3E3",
+        plot_bgcolor="#FFFFFF",
+        # width=900,
+        # height=1000,
+        # title={'text' : f"SDG 11.2.1 assessment and related value by selecting province in Thailand"
+        #     ,'x': 0.5, # Set the x anchor to the center of the chart
+        #     'xanchor': 'center'},
+        margin=dict(l=50, r=50, t=50, b=50),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+        xaxis_title='Types of emission detected by MODIS ',
+        yaxis_title='Amount of emission in Kg',
+        font=dict(
+            color='black',
+        ),
+        xaxis=dict(
+            title_font=dict(
+                color='black',
+            ),
+            tickfont=dict(
+                color='black',
+            )
+        ),
+        yaxis=dict(
+            title_font=dict(
+                color='black',
+            ),
+            tickfont=dict(
+                color='black',
+            )
+        )
+    )
+    st.plotly_chart(fig4, use_container_width=True)
 
 # =============================================================================
 # Bar plot comparing MODIS and VIIRS
@@ -438,13 +448,3 @@ fig7.update_layout(
     )
 )
 st.plotly_chart(fig7, use_container_width=True)
-
-
-# =============================================================================
-# Types of Landuse
-# =============================================================================
-# data = {'LU CODE' : ['A101' , 'A202', 'F101', 'F201'],
-#         'DESCRIPTION' : ['Active paddy field', 'Corn', 'Dense evergreen forest', 'Dense deciduous forest']}
-# data = pd.DataFrame(data)
-# data.set_index('LU CODE', inplace=True)
-# st.dataframe(data)
